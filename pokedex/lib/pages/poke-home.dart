4b2d738888d6342c6bd5b4e05fd07008b5fae3c3
emilioheinz <emiliosheinz@gmail.com>
2020-02-16 16:29:48 -0300

    Fixing navigation structure

--- pokedex/lib/pages/poke-home.dart
+++ pokedex/lib/pages/poke-home.dart
@@ -1,13 +1,7 @@
-import 'dart:convert';
-import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex/components/poke-card.dart';
-import 'package:pokedex/components/poke-loader.dart';
-import 'package:pokedex/enums/pokemon-type-enum.dart';
-import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/pages/poke-list.dart';
 import 'package:pokedex/res/my-colors.dart';
-import 'package:pokedex/services/api.dart';
 
 class PokeHomePage extends StatefulWidget {
   @override
@@ -15,49 +9,6 @@ class PokeHomePage extends StatefulWidget {
 }
 
 class _PokeHomePageState extends State<PokeHomePage> {
-  List<PokemonModel> pokemonsList;
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _getPokemons();
-  }
-
-  _getPokemons() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    List<PokemonModel> pokemons = [];
-    Response resp = await PokeApi().listPokemons();
-
-    final pokemonsFromApi = JsonDecoder().convert(resp.toString());
-
-    pokemonsFromApi['pokemons'].forEach((pokemon) {
-      List<PokeTypeEnum> typesList = [];
-
-      pokemon['types'].forEach((type) {
-        int enumIndex = PokemonModel.getEnumIndexFromPokeType(type);
-        typesList.add(PokeTypeEnum.values[enumIndex]);
-      });
-
-      pokemons.add(PokemonModel.fromJson(pokemon, typesList));
-    });
-
-    setState(() {
-      pokemonsList = pokemons;
-      isLoading = false;
-    });
-  }
-
-  Widget _buildPokeCards(BuildContext context, int index) {
-    return PokeCard(
-      pokemon: pokemonsList[index],
-    );
-  }
-
   _renderAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
@@ -82,34 +33,11 @@ class _PokeHomePageState extends State<PokeHomePage> {
     );
   }
 
-  _renderContent() {
-    if (isLoading) {
-      return Center(
-        child: SizedBox(
-          height: 25,
-          width: 25,
-          child: PokeLoader(),
-        ),
-      );
-    }
-
-    return Container(
-      child: ListView.separated(
-        itemBuilder: _buildPokeCards,
-        itemCount: pokemonsList.length,
-        separatorBuilder: (context, index) => Divider(
-          color: MyColors.dustyGray,
-        ),
-      ),
-    );
-  }
-
   _renderBottomTab() {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       backgroundColor: MyColors.appBar01.withOpacity(0.8),
       selectedItemColor: Colors.black,
-      currentIndex: 0,
       items: [
         BottomNavigationBarItem(
           icon: SizedBox(
@@ -143,7 +71,7 @@ class _PokeHomePageState extends State<PokeHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _renderAppBar(),
-      body: _renderContent(),
+      body: PokeListPage(),
       bottomNavigationBar: _renderBottomTab(),
     );
   }
---
+++ pokedex/lib/pages/poke-list.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/components/poke-card.dart';
+import 'package:pokedex/components/poke-loader.dart';
+import 'package:pokedex/enums/pokemon-type-enum.dart';
+import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/res/my-colors.dart';
+import 'package:pokedex/services/api.dart';
+
+class PokeListPage extends StatefulWidget {
+  @override
+  _PokeListPageState createState() => _PokeListPageState();
+}
+
+class _PokeListPageState extends State<PokeListPage> {
+  List<PokemonModel> pokemonsList;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getPokemons();
+  }
+
+  _getPokemons() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    List<PokemonModel> pokemons = [];
+    Response resp = await PokeApi().listPokemons();
+
+    final pokemonsFromApi = JsonDecoder().convert(resp.toString());
+
+    pokemonsFromApi['pokemons'].forEach((pokemon) {
+      List<PokeTypeEnum> typesList = [];
+
+      pokemon['types'].forEach((type) {
+        int enumIndex = PokemonModel.getEnumIndexFromPokeType(type);
+        typesList.add(PokeTypeEnum.values[enumIndex]);
+      });
+
+      pokemons.add(PokemonModel.fromJson(pokemon, typesList));
+    });
+
+    setState(() {
+      pokemonsList = pokemons;
+      isLoading = false;
+    });
+  }
+
+  Widget _buildPokeCards(BuildContext context, int index) {
+    return PokeCard(
+      pokemon: pokemonsList[index],
+    );
+  }
+
+  _renderContent() {
+    if (isLoading) {
+      return Center(
+        child: SizedBox(
+          height: 25,
+          width: 25,
+          child: PokeLoader(),
+        ),
+      );
+    }
+
+    return Container(
+      child: ListView.separated(
+        itemBuilder: _buildPokeCards,
+        itemCount: pokemonsList.length,
+        separatorBuilder: (context, index) => Divider(
+          color: MyColors.dustyGray,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _renderContent();
+  }
+}
